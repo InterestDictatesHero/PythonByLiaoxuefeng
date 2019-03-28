edc5abc6b77fb50c0f306c278e9bc7e123abda12
InterestDictatesHero <1553035261@qq.com>
2019-03-28 08:48:51 +0800

    迭代器提交

--- 高级特性/迭代器.py
+++ 高级特性/迭代器.py
@@ -44,8 +44,55 @@
 from collections import Iterable
 
 def main():
-    print(isinstance([],Iterable))
-    print(isinstance({},Iterable))
+    print('isinstance([],Iterable):',isinstance([],Iterable))
+    print('isinstance({},Iterable):',isinstance({},Iterable))
+    print('isinstance("abc",Iterable):',isinstance('abc',Iterable))
+    print('isinstance((x for x in range(10)),Iterable):',isinstance((x for x in range(10)),Iterable))
+    print('isinstance(100,Iterable):',isinstance(100,Iterable))
+
+    """
+        `生成器`不但可以作用于 `for` 循环，还可以被 `next()` 函数不断调用并返回下一个值，
+        直到最后抛出 `StopIteration` 错误表示无法继续返回下一个值
+
+        可以被 `next()` 函数调用并不断返回下一个值得对象称为迭代器： `Iterator`
+
+        可以使用 `isinstance()` 判断一个对象是否是 `Iterator` 对象
+    """
+    from collections import Iterator
+    print('isinstance([],Iterator):',isinstance([],Iterator))
+    print('isinstance({},Iterator):',isinstance({},Iterator))
+    print('isinstance("abc",Iterator):',isinstance('abc',Iterator))
+    print('isinstance((x for x in range(10)),Iterator):',isinstance((x for x in range(10)),Iterator))
+
+    print('-----------------------------------------------------------------')
+    """
+        生成器都是 `Iterator`对象，但 `list`、`dict`、`str`虽然是 `Iterable`，却
+        不是 `Iterator` 
+
+        把 `list`、`dict`、`str`等`Iterable`变成`Iterator`可以使用 `iter()` 函数
+    """    
+    print('isinstance(iter([]),Iterator):  ',isinstance(iter([]),Iterator))
+    print('isinstance(iter(\'abc\'),Iterator):  ',isinstance(iter('abc'),Iterator))
+
+
+    """
+        这是因为 Python 的 Iterator 对象表示的是一个数据流，
+        Iterator 对象可以被 next() 函数调用并不断返回下一个数据，直到没有数据是抛出
+    StopIteration 错误。
+        可以把这个数据流看做一个有序序列，但我们却不能提前知道序列的长度，只能不断通过
+    next() 函数实现按需计算下一个数据，所以 Iterator 的计算式惰性的，只有在需要返回下
+    一个数据时它才会计算。
+
+        Iterator 甚至可以表示一个无限大的数据流，例如全体自然数。
+        而使用 list 是永远不可能存储全体自然数的
+    """
+
+    """
+        1. 凡是可作用于 `for` 循环的对象都是 `Iterable`类型;
+        2. 凡是可用于 `next()` 函数的对象都是 `Iterator` 类型，它们表示一个惰性计算的序列；
+        3. 集合数据类型 如 `list`、 `dict`、 `str` 等是 `Iterable` 但不是 `Iterator`,
+           不过可以通过 `iter()` 函数获得一个 `Iterator` 对象。
+    """
 
 if __name__ == "__main__":
     main()
